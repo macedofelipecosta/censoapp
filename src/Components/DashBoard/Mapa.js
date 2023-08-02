bb1cb1d7dc5640917d59b1e1bcbfc0d13d300285
Felipe Macedo Costa <128637344+macedofelipecosta@users.noreply.github.com>
2023-08-02 19:21:42 -0300

    020823

--- src/Components/DashBoard/Mapa.js
+++ src/Components/DashBoard/Mapa.js
@@ -1,13 +1,14 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import { useState } from 'react';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
 
@@ -17,20 +18,22 @@ L.Icon.Default.mergeOptions({
 
 
 const Mapa = () => {
+
+
   return (
     <div>
 
-<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{height:'300px', width:'50%'}}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
+      <MapContainer center={[-32.522779, -55.765835]} zoom={6} scrollWheelZoom={true} style={{  height: '300px', width: '50%' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[-32.522779, -55.765835]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
 
     </div>
   )
